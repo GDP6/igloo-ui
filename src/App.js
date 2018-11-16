a78b25d2268f2f72bc7256ba22e50f0826186ec0
Mikolaj <mfb2g15@soton.ac.uk>
2018-11-16 12:34:21 +0000

    Added sliding panel

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 import HeaderBar from './components/HeaderBar';
 import TankInformation from './components/TankInformation';
+import { render } from 'react-dom';
+import Modal from 'react-modal';
+import SlidingPane from 'react-sliding-pane';
+import 'react-sliding-pane/dist/react-sliding-pane.css';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      isPaneOpen: false
+    };
+  }
+
+  componentDidMount() {
+    Modal.setAppElement(this.el);
+  }
+
+
   render() {
     return (
       <div className="App">
         <HeaderBar />
+        <button onClick={ () => this.setState( {isPaneOpen: true} ) } />
         <TankInformation 
           time="20 minutes"
-          percentage="100"
+          percentage="90"
         />
+        <SlidingPane
+          isOpen={ this.state.isPaneOpen }
+          title='Some Title'
+          subtitle='Some subtitle'
+          from='left'
+          width='200px'
+          onRequestClose={ () => {
+            this.setState({ isPaneOpen: false});
+          }} />
       </div>
     );
   }
