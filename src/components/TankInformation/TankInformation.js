183a64eccedb9bf9840560b989390e30e239567c
WhiteSeal <miki.bak0221@gmail.com>
2019-01-12 18:38:52 +0000

    bug fixes

--- src/components/TankInformation/TankInformation.js
+++ src/components/TankInformation/TankInformation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './TankInformation.css';
+import tank from './tank.png';
 
 
-const TankInformation = ({time, percentage}) => {
+const TankInformation = ({time, percentage, name}) => {
 
 	return (
 		<div className='tankInfo'>
-			<h1>There's about {time} of hot water in your tank.</h1>
+			<h1>Hi {name}! There's about {time} of hot water in your tank.</h1>
 			<div className="tankTop"> 
 				<h1>{percentage}%</h1>
 			</div>
